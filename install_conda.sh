a4887a904f1bed15067ad978f8d7894fa617a80f
alvin6233 <dong.alvinme@gmail.com>
2023-06-07 13:11:27 +0800

    Update install_conda.sh

--- install_conda.sh
+++ install_conda.sh
@@ -4,7 +4,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -f -p $HOME/miniconda
 ls $HOME/miniconda
-echo "$HOME/miniconda/etc/profile.d/conda.sh" >> $HOME/.bashrc
+cat $HOME/miniconda/etc/profile.d/conda.sh >> $HOME/.bashrc
 cat $HOME/.bashrc
 source $HOME/.bashrc >> /dev/null
 conda init bash
