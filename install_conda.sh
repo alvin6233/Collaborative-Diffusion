e60c767566cf1b7f785644ae0fc09803fcb70605
alvin6233 <dong.alvinme@gmail.com>
2023-06-07 13:27:19 +0800

    Update install_conda.sh

--- install_conda.sh
+++ install_conda.sh
@@ -5,7 +5,7 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -f -p $HOME/miniconda
 ls $HOME/miniconda
 cat $HOME/miniconda/etc/profile.d/conda.sh >> $HOME/.bashrc
-cat $HOME/.bashrc
-source $HOME/.bashrc
+chmod 777 .bashrc
+cat $HOME/.bashrc & source $HOME/.bashrc
 conda init bash
 
